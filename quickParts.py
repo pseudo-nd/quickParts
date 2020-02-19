187100e4bdefe2456bff4dd8e9b67d2574a89c72
pseudo-nd <52451683+pseudo-nd@users.noreply.github.com>
2020-02-18 18:23:49 -0600

    updated with brandt

--- quickParts.py
+++ quickParts.py
@@ -1,7 +1,7 @@
-#quickParts.py
-#!/usr/bin/env python
-#!C:\Python27\python.exe
 # -*- coding: utf-8 -*-
+#!/usr/bin/env python
+#!C:\Python37_64\python.exehow m
+#quickParts.py
 import sys
 import os
 import time
@@ -9,6 +9,8 @@ import webbrowser
 import jdfilters
 import catalogs
 import misc
+import app
+import macdon
 
 global question
 global answer
@@ -21,26 +23,32 @@ def main():
 
 def answer():
         try:
-                question = raw_input("How can I help you?: ")
-                if 'filter'.lower() in question:
+                question = input("How can I help you?: ")
+                if 'filter'.casefold() in question:
                         ask_a_question(question)
-                elif 'filters'.lower() in question:
+                elif 'filters'.casefold() in question:
                         ask_a_question(question)
-                elif 'catalog'.lower() in question:
+                elif 'catalog'.casefold() in question:
                         ask_a_question2(question)
-                elif 'exit'.lower() in question:
-                        exit(question)
-                elif 'how'.lower() in question:
+                elif 'show'.casefold() in question:
                         ask_a_question3(question)
-                elif 'what'.lower() in question:
+                elif 'what'.casefold() in question:
                         ask_a_question3(question)
-                elif 'show'.lower() in question:
+                elif 'how'.casefold() in question:
                         ask_a_question3(question)
-                elif 'macdon'.lower() in question:
+                elif 'app'.casefold() in question:
                         ask_a_question4(question)
-                else:
-                        print('')
-                        raise Exception("------------------------------------- \n[I do not understand that] \n-------------------------------------")
+                elif 'manual'.casefold() in question:
+                        ask_a_question5(question)
+                elif 'macdon'.casefold() in question:
+                        ask_a_question6(question)
+                elif 'brandt'.casefold() in question:
+                        ask_a_question7(question)
+                elif 'exit'.casefold() in question:
+                        exit(question)
+                
+
+                
                         
         except:
                 print("------------------------------------- \n[I do not understand that] \n-------------------------------------")
@@ -49,17 +57,30 @@ def answer():
                 answer()
         
 def ask_a_question(answer):
-        jdfilters.filter_question(answer)
-def ask_a_question2(amswer):
-        catalogs.catalog_question(answer)
+                jdfilters.filter_question(answer)
+
+def ask_a_question2(answer):
+                catalogs.catalog_question(answer)
+
 def ask_a_question3(answer):
-        misc.misc_question(answer)
+                misc.misc_question(answer)
+
 def ask_a_question4(answer):
-        macdon.macdon_question(answer)
-def exit(answer):
-        print("Exiting quickParts.....", answer)
+                app.app_question(answer)
+
+def ask_a_question5(answer):
+                manuals.manuals_question(answer)
+
+def ask_a_question6(answer):
+                macdon.macdon_question(answer)
+
+def ask_a_question7(answer):
+                brandt.brandt_question(answer)
+
+def exit(question):
+        print("Exiting quickParts.....", question)
         time.sleep(1.5)
-        exit(answer)
+        os._exit(0)
         
 if __name__ == "__main__":
         main()
